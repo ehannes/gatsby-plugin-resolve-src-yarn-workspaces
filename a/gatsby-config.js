bbf8b05aae9b40c192aa2ff98112ca3ac830c89e
Hannes Elvemyr <hannes@accodeing.com>
2019-11-13 10:52:06 +0100

    Initial commit
    This is a new gatsby site which has been moved to the `a` workspace.
    I have done the following:
    - Removed gatsby-plugin-manifest (since I had some building problems
    with Sharp, never mind that)
    - Added the gatsby-plugin-resolve-src plugin and it's configuration in
    gatsby-config
    - Changed the link in src/components/layout.js to use relative path (not
    working)

--- a/gatsby-config.js
+++ a/gatsby-config.js
@@ -5,6 +5,7 @@ module.exports = {
     author: `@gatsbyjs`,
   },
   plugins: [
+    "gatsby-plugin-resolve-src",
     `gatsby-plugin-react-helmet`,
     {
       resolve: `gatsby-source-filesystem`,
